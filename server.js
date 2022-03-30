e12e6641eab1730551575f298696b2ba5fe9d7c6
mattrossman <rossman@gatech.edu>
2022-03-30 18:59:14 -0400

    Pass server to createViteServer

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require("express")
 const { createServer: createViteServer } = require("vite")
+const { Server } = require("http")
 
 const PORT = process.env.PORT || 3000
 
 async function createServer() {
   const app = express()
+  const server = new Server(app)
 
   const vite = await createViteServer({
-    server: { middlewareMode: "html" },
+    server: { middlewareMode: "html", hmr: { server } },
   })
 
   app.use(vite.middlewares)
 
-  app.listen(PORT, async () => {
+  server.listen(PORT, async () => {
     console.log(`listening on http://localhost:${PORT}`)
   })
 }
